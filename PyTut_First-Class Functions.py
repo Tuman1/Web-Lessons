e62ab333b03f3c257bcb4d1ee31603fae0f2cb2f
Tuman1 <shulika.ilya@gmail.com>
2017-05-18 23:33:23 +0300

    Add some Clousers function

--- PyTut_First-Class Functions.py
+++ PyTut_First-Class Functions.py
@@ -1,16 +1,42 @@
 # Programming Terms: First-Class Functions
-def square(x):
-    return x*x
 
-def cube(x):
-    return x * x * x
+# def square(x):
+#     return x*x
+#
+# def cube(x):
+#     return x * x * x
+#
+# def my_map(func, arg_list):
+#     result = []
+#     for item in arg_list:
+#         result.append(func(item))
+#     return result
+#
+# square = my_map(cube, range(1,6))
+#
+# print(square)
 
-def my_map(func, arg_list):
-    result = []
-    for item in arg_list:
-        result.append(func(item))
-    return result
+##Example of Closures
+# def logger(msg):
+#
+#     def log_message():
+#         print("Log: ", msg)
+#
+#     return log_message
+#
+# log_hi = logger('Hi!')
+# log_hi()
 
-square = my_map(cube, range(1,6))
+def html_tag(tag):
 
-print(square)
+    def warp_text(msg):
+        print('<{0}><{1}><\{0}>'.format(tag, msg))
+
+    return warp_text
+
+print_h1 = html_tag('h1')
+print_h1('Test Headline!')
+print_h1('Another Headline!')
+
+print_p = html_tag('p')
+print_p('Test Paragraph!')
