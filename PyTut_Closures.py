349287590ea7f976e0732b6b2396f35eec157f65
Tuman1 <shulika.ilya@gmail.com>
2017-05-20 19:21:43 +0300

    Basic info CS

--- PyTut_Closures.py
+++ PyTut_Closures.py
@@ -1,1 +1,16 @@
 # Programming Terms: Closures - How to Use Them and Why They Are Useful
+
+# Closures
+def outer_func(msg):
+    message = msg
+
+    def inner_func():
+        print(message)
+
+    return inner_func
+
+hi_func = outer_func('hi')
+hello_func = outer_func('hello')
+
+hi_func()
+hello_func()
